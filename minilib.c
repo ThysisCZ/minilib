00f4360ec7aca1c5ee554a933152c56fde2b3cc7
Jiří Kerner <jirikernerjr@gmail.com>
2026-09-06 21:09:48 +0200

    Minimum and maximum

--- minilib.c
+++ minilib.c
@@ -1,10 +1,17 @@
 #define MINILIB_IMPLEMENTATION
 #include "minilib.h"
 
+#define ARRAY_LENGHT(array) ((int)(sizeof(array) / sizeof(array[0])))
+
 int main()
 {
-    const char *message = hello_world();
-    printf("%s", message);
+    int array[5] = {3, 4, 0, -2, -1};
+
+    int min = arr_min(array, ARRAY_LENGHT(array));
+    printf("%d\n", min);
+
+    int max = arr_max(array, ARRAY_LENGHT(array));
+    printf("%d\n", max);
 
     return 0;
 }
--- minilib.h
+++ minilib.h
@@ -2,21 +2,47 @@
 #define MINILIB_H_
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #ifndef MLDEF
 #define MLDEF static inline
 #endif // MLDEF
 
-MLDEF const char *hello_world();
+MLDEF int arr_min(int array[], int length);
+MLDEF int arr_max(int array[], int length);
 
 #endif // MINILIB_H_
 
 #ifdef MINILIB_IMPLEMENTATION
 
-MLDEF const char *hello_world()
+MLDEF int arr_min(int array[], int length)
 {
-    const char *message = "Hello world!";
-    return message;
+    int minimum = array[0];
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] < minimum)
+        {
+            minimum = array[i];
+        }
+    }
+
+    return minimum;
+}
+
+MLDEF int arr_max(int array[], int length)
+{
+    int maximum = array[0];
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > maximum)
+        {
+            maximum = array[i];
+        }
+    }
+
+    return maximum;
 }
 
 #endif // MINILIB_IMPLEMENTATION
